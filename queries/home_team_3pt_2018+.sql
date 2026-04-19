2ce152c2bd674c799b43ddb989ea97e59feb6b9b
IsaacD <isaacdoggett@my.unt.edu>
2026-04-19 17:40:45 -0500

    Create home_team_3pt_2018+.sql
    Write a query that returns the average home team win rate and average three-point percentage at home from the year 2018 to now, grouped by home team name and season.

---
+++ queries/home_team_3pt_2018+.sql
@@ -0,0 +1,9 @@
+SELECT
+    team_home AS home_team,
+    season,
+    AVG(home_team_win) AS avg_home_win_rate,
+    AVG(pct_3p_home) AS avg_home_3pt_percentage
+FROM nba.games
+WHERE season >= '2018'
+GROUP BY team_home, season
+ORDER BY team_home, season;
