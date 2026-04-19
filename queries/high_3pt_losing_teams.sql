da235b7f839e61153c916456adc8e43639e2c277
IsaacD <isaacdoggett@my.unt.edu>
2026-04-19 17:45:24 -0500

    Create high_3pt_losing_teams.sql
    Add an additional condition to your query from high_3pt_teams_count.sql to filter to teams with a losing record (win rate < 0.5), in addition to having a high three-point shot rate.

---
+++ queries/high_3pt_losing_teams.sql
@@ -0,0 +1,14 @@
+SELECT COUNT(*) AS num_teams
+FROM (
+    SELECT
+        team_home AS home_team,
+        season,
+        AVG(home_team_win) AS avg_home_win_rate,
+        AVG(pct_3p_home) AS avg_home_3pt_percentage
+    FROM nba.games
+    WHERE season >= '2018'
+    GROUP BY team_home, season
+    HAVING
+        AVG(pct_3p_home) >= 0.37 AND
+        AVG(home_team_win) < 0.5
+) AS filtered_teams;
